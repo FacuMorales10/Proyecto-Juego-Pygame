4f685b9a1993b6a5a349d39512930dafe3bf0a65
Lucas Bricchi <lucasbricchi12@gmail.com>
2025-07-11 23:12:43 -0300

    Agrego musica Inicio

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pygame
 from screens.menu import mostrar_menu_principal
 from intro import mostrar_intro
 import settings as st
+from music import play_music_menu 
 
 if __name__ == "__main__":
     pygame.init()
+    pygame.mixer.init()  
+    pygame.mixer.music.set_volume(0.4)
+
+    play_music_menu()  
 
     pantalla = pygame.display.set_mode((st.ANCHO_VENTANA, st.ALTO_VENTANA))
     pygame.display.set_caption("Mach-Max")
